91257a31f6f2970a9f1cb635f55da76ee64cc229
TanjodhHayer <TanjodhHayer@gmail.com>
2024-02-08 19:46:58 -0800

    basic website porfolio done

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// script.js
+
+function showProject(projectName) {
+    // Redirect to the generic project page with the project name as a parameter
+    window.location.href = `project.html?project=${projectName}`;
+}
