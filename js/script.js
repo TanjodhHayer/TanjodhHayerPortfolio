281cf22394a1375ede4648ad471d3546c5abb0e9
TanjodhHayer <TanjodhHayer@gmail.com>
2024-02-08 19:54:00 -0800

    basics complete

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-
-function showProject(projectName) {
-    // Redirect to the generic project page with the project name as a parameter
-    window.location.href = `project.html?project=${projectName}`;
-}
